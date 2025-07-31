8d84576bea25ec7c28cfae681afc3857bc31a2d5
Isaac Antwi <antwikisaac@gmail.com>
2025-07-31 10:12:34 +0000

    Created the payment model.

---
+++ models/paymentModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    orderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+        required: true,
+    },
+    paystackRef: {
+        type: String,
+        required: true,
+    },
+    amout: {
+        type: Number,
+        required: true,
+    },
+    currency: {
+        type: String,
+        default: "GHS",
+    },
+    status: {
+        type: String,
+        enum: ["success", "failed", "pending"],
+        default: "pending"
+    },
+    paymentResponse: {
+        type: Object,
+    },
+},
+{
+    timestamps: true,
+});
